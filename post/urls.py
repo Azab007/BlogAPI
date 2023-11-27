7ed99a700bbda91d1038d28e627ed53b705f930b
Azab007 <mazab322@gmail.com>
2023-11-27 02:28:03 +0200

    hotfix

--- post/urls.py
+++ post/urls.py
@@ -4,8 +4,6 @@ from .views import PostViewSet, CommentViewSet
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet, basename="post")
-router.register(r'comments', CommentViewSet, basename="comment")
-
 urlpatterns = [
     path('api/posts/<int:post_pk>/comments/', CommentViewSet.as_view({'get': 'list', 'post': 'create'}), name='post-comments'),
     path('api/', include(router.urls)),
